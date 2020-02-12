cf7a272c14e4131060a4395574838d6b70852482
oren amema <orenamema@gmail.com>
2020-02-12 12:03:40 -0800

    editing api routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,27 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import API from "./utils/API";
+
+class App extends Component {
+  state = {
+    books: [],
+    bookSearch: ""
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    API.getBooks(this.state.bookSearch)
+      .then(res => this.setState({ books: res.data.hits }))
+      .catch(err => console.log(err));
+  };
 
 function App() {
   return (
