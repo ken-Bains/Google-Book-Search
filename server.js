d012b93ef46f5d532c4ba1f6ea566e18b775e854
KB <nkbains10@gmail.com>
2020-02-12 11:43:41 -0800

    added mongo database. added seeds. added querys to database

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,10 +12,12 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBooks");
 
 // Define API routes here
 
 // Send every other request to the React app
+
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
